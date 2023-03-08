7b15b62bcc8111aa90f39b8b3de4bcd849b925a8
higsch <matthias@higsch.com>
2023-03-08 15:22:05 +0100

    use bremen radiation data

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,8 @@
 import { tsvParse, autoType } from 'd3';
-import { getDayOfYear } from '$lib/solar';
 
 export async function load({ fetch }) {
-  const responseRadiation = await fetch('/radiation_hamburg_2022_k10.tsv');
+  const responseRadiation = await fetch('/radiation_bremen_2022_k10.tsv');
+  // const responseRadiation = await fetch('/radiation_hamburg_2022_k10.tsv');
   // const responseRadiation = await fetch('/radiation_mannheim_2020_k10.tsv');
   // const responseRadiation = await fetch('/radiation_stockholm_2019_k60.tsv');
   // const responseRadiation = await fetch('/radiation_weihenstephan_2013_k10.tsv');
